2eadc362b42dd25fabccdf0a77d91736c461cc52
Loki <riqc0nt4ct@hotmail.com>
2022-07-06 14:27:00 -0300

    +++

---
+++ intro-progr/lista-treinamento/easy/nickname.py
@@ -0,0 +1,6 @@
+fisrt_name = str(input())
+last_name = str(input())
+
+nickname = fisrt_name + last_name
+
+print(nickname)
---
+++ intro-progr/lista-treinamento/easy/oferenda.py
@@ -0,0 +1,10 @@
+diamonds = int(input())
+
+if diamonds <= 10:
+    print('Arhur')
+elif diamonds <= 30:
+    print('Luiz')
+elif diamonds <= 100:
+    print('Pedro')
+else:
+    print('Nenhum')
