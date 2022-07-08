abb81d64cb5cd5b3cb6ef8408b3657ad4591098a
Loki <riqc0nt4ct@hotmail.com>
2022-07-08 09:29:43 -0300

    221L1Q1 finished

---
+++ intro-progr/lista-comandos-condicionais/221L1Q01_easy.py
@@ -0,0 +1,11 @@
+food = str(input())
+
+if food == 'Canjica':
+    print('Boa! Canjica faz parte.')
+elif food == 'Pamonha':
+    print('Boa! Pamonha faz parte.')
+elif food == 'Bolo de milho':
+    print('Boa! Bolo de milho faz parte.')
+else:
+    print('Opa! Parece que voce procura um\ntipo de comida que nao combina\ncom essa epoca, tente novamente.')
+    
---
+++ intro-progr/lista-comandos-condicionais/221L1Q0_easy.py
@@ -0,0 +1,14 @@
+number = int(input())
+
+if number == 1:
+    print('Você tirou o Doguinho! Vai receber muitos lambeijos!')
+elif number == 2:
+    print('Gabriel Queiroz, eu escolho você! Aí vem o galã da monitoria te tascar um beijão!')
+elif number == 3:
+    print('Que azar! Não vai beijar ninguém! Tome uma língua de gato de prêmio de consolação…')
+elif number == 4:
+    print('Olha só! O misterioso monitor de branco vem te entregar uma lista para fazer e um selinho de encorajamento!')
+elif number == 5:
+    print('Ca-Calegário?!!! Ganhou um Hi-5 e um projeto de IP/P1 para entregar daqui a duas semanas!')
+else:
+    print('Véi! São 5 opções, não é difícil escolher…')
