a2674fd06e1e21090322e1e80bbaadc2aa788208
Loki <riqc0nt4ct@hotmail.com>
2022-07-13 11:18:03 -0300

    221L1Q05 finished

---
+++ intro-progr/lista-comandos-condicionais/221L1Q05_easy.py
@@ -0,0 +1,14 @@
+comando = input()
+
+if comando == 'Olha a chuva!':
+    print('Grite choveu!')
+elif comando == 'Olha a cobra!':
+    print('Grite é mentira!')
+elif comando == 'Cavalheiros ajoelhados de frente pras damas!':
+    print('Ajoelhe-se na frente da dama, beije a mão dela e grite beijou!')
+elif comando == 'Alavantu!':
+    print('Vá em direção a sua dama')
+elif comando == 'Anarriê!':
+    print('Se distancie da sua dama')
+elif comando == 'Balancê!':
+    print('Dance com a sua dama')
